bcf3581d09cbf660d345d1cc4f300ccc52d5abd3
Eva De Haro <87303447+evadeharomunoz@users.noreply.github.com>
2024-11-11 19:46:48 +0100

    chore: base enviroment

---
+++ test/src/App.tsx
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { ReactNode, useRef } from 'react';
+
+type CameraMovementProps = {
+  children: ReactNode;
+};
+
+function CameraMovement({ children }: CameraMovementProps) {
+  const { pointer } = useThree();
+  const vec = new THREE.Vector3();
+  const cam = useRef<THREE.Group>(null);
+
+  useFrame(() => {
+    cam.current?.position.lerp(
+      vec.set(pointer.x * -0.001, pointer.y * -0.001, 0),
+      -0.001
+    );
+  });
+
+  return <group ref={cam}>{children}</group>;
+}
+
+function App() {
+  return (
+    <Canvas camera={{ fov: 60, position: [0, 0, 20] }}>
+      <OrbitControls />
+      <CameraMovement>
+        <mesh>
+          <boxGeometry args={[5, 5, 5]} />
+          <meshStandardMaterial color='orange' />
+        </mesh>
+      </CameraMovement>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+    </Canvas>
+  );
+}
+
+export default App;
