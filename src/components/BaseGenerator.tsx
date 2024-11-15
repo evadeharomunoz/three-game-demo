91a33df509085bf55e4032ede24fb45d91297c7f
Eva De Haro <87303447+evadeharomunoz@users.noreply.github.com>
2024-11-15 09:48:55 +0100

    feat: added utils and first map

---
+++ src/components/BaseGenerator.tsx
@@ -0,0 +1,21 @@
+import { TileInformation } from './types/types';
+import { BoxGeometry } from 'three';
+
+import getTileInformation from '../utils/getTileInformation';
+
+const cube = new BoxGeometry(1, 1, 1);
+
+function BaseGenerator(props: TileInformation) {
+  const spacing = 1.05;
+
+  return (
+    <mesh
+      key={`${props.x}-${props.y}-${props.z}`}
+      position={[props.x * spacing, props.y * spacing, props.z * spacing]}
+      material={getTileInformation(props.type).material}
+      geometry={cube}
+    />
+  );
+}
+
+export default BaseGenerator;
